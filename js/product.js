e07eefd74a520605034bba9a407d0fd36b6a203c
Kamilla Huhnke <kaan0001@stud.kea.dk>
2023-08-28 14:29:18 +0200

    java virkerrrr

--- js/product.js
+++ js/product.js
@@ -1,20 +1,24 @@
 // https://kea-alt-del.dk/t7/api/products/1573
-fetch("https://kea-alt-del.dk/t7/api/products/1573")
-        .then(response=>response.json())
-        .then((data)=>showProduct(data));
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
 
-function showProduct(product){
-console.log (product);
-document.querySelector(".product1 h3").textContent=product.productdisplayname;
-document.querySelector(".product1 .brand").textContent=product.brandname;
-document.querySelector(".product1 .categorie").textContent=product.articletype;
-document.querySelector(".product1 .price").textContent=product.price;
-document.querySelector(".product1 .discount").textContent=product.discount;
-document.querySelector("img").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".product1 h3").textContent =
+    product.productdisplayname;
+  document.querySelector(".product1 .brand").textContent = product.brandname;
+  document.querySelector(".product1 .categorie").textContent =
+    product.articletype;
+  document.querySelector(".product1 .price").textContent = product.price;
+  document.querySelector(".product1 .discount").textContent = product.discount;
+  document.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 }
-
-
-
 
 /*
 	
